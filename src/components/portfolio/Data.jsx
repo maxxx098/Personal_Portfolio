2d91279be8d392a72558f6e6c5e926774c07e871
symon <simmonswalker79@gmail.com>
2024-08-21 01:29:10 +0800

    first commit

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,88 @@
+
+import Work9 from '../../assets/image.png'
+import Work7 from '../../assets/work7.png'
+import Work8 from '../../assets/work8.png'
+import Work10 from '../../assets/Eccommerce.jpg'
+import Sy from '../../assets/Portfolio.jpg'
+import Sy1 from '../../assets/sy1.png'
+import Sy3 from '../../assets/sysysy.png'
+
+
+
+  export const projectsData = [
+    
+    {
+      id: 1,
+      image: Work7,
+      title: "Barber Shop Website",
+      description:"Language Used: React JS",
+      category: "Web",
+      URL: "https://maxxx098.github.io/Barber_Shop_Template/",
+    },
+    {
+      id: 2,
+      image: Work8,
+      title: "Portfolio Website",
+      description:"Language Used: HTML | CSS | JS",
+      category: 'UI | UX',
+      URL: "https://maxxx098.github.io/Portfolio/portfolio/",
+    },
+    {
+      id: 3,
+      image: Work9,
+      title: "Hotel Booking Website",
+      description:"Language Used: Laravel | PHP",
+      category: "Full Stack",
+    },
+    {
+      id: 4,
+      image: Work10,
+      title: "Tech Eccomerce Website",
+      description:"Language Used: React / Node JS | Paylaod CMS",
+      category: 'Full Stack',
+      URL: "https://e-commerce-sitte.payloadcms.app/",
+    },
+    {
+      id: 5,
+      image: Sy,
+      title: "Own Portfolio Website",
+      description:"Language Used: React / Node JS | Github",
+      category: 'UI | UX',
+      URL: "https://maxxx098.github.io/Updated_Portfolio/",
+    },
+    {
+      id: 6,
+      image: Sy1,
+      title: "Brainwave Chat Bot",
+      description:"Language Used: React / Node JS / Tailwwind",
+      category:'UI | UX',
+      URL: "https://jsm-brainwave.com/",
+    },
+    {
+      id: 7,
+      image: Sy3,
+      title: "Social App",
+      description:"Language Used: React / Node JS / Tailwwind / Appwrite/ TypeScript",
+      category:'UI | UX',
+      URL: "https://snapgram-one.vercel.app/",
+    },
+  ];
+
+  export const projectsNav = [
+    {
+        name: 'all',
+        
+    },
+    {
+        name:'UI | UX',
+    },
+    {
+        name:'Full Stack',
+    },
+    {
+      name:'Web',
+  }
+    
+
+    
+  ];
---
+++ src/components/testimonials/data.jsx
@@ -0,0 +1,13 @@
+import Image4 from '../../assets/Hannah.jpg'
+
+export const Data = [
+    {
+      id: 1,
+      image: Image4,
+      title: "Hannah Ferrera",
+      description:
+        "The web developer values time and commits to working as stated in the contract, ensuring that the client's expectations won't turn into disappointments.",
+      JobTitle: "Client",
+      Date:"May 11, 2024",
+    }
+  ];
